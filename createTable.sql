9282f6ebee3d3850b1f9ae5ba50ac2cef3c3d177
unclebiglu <1807814821@qq.com>
2021-05-11 19:09:32 +0800

    First commit

---
+++ createTable.sql
@@ -0,0 +1,6 @@
+CREATE TABLE todolist(
+    event_name text NOT NULL,
+    todo_time timestamp NOT NULL,
+    is_done boolean NOT NULL,
+    create_time timestamp PRIMARY KEY
+);
---
+++ myRegex.py
@@ -0,0 +1,21 @@
+import re
+
+test_list = [
+    '提醒 今天下午14点干一件事',
+    '提醒 3月29日1点干啥',
+    '提醒 5点干啥'
+]
+
+pattern = r'^提醒\s((今天|明天|后天)|((20\d{2}年)?(1?\d月)?([1-3]?\d日)))?(上午|下午)?([1-2]?\d点)?'
+re_time = re.compile(pattern)
+
+# group2: 今天 明天 后天
+# group4: 年
+# group5: 月
+# group6: 日
+# group7: 上午 下午
+# group8: 点
+
+m = re_time.match(test_list[0])
+print(m.group(7))
+
