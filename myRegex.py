860a67d7b804d25d852cfed6d995729094c41db6
unclebiglu <1807814821@qq.com>
2021-05-12 08:18:31 +0800

    parse_input function sealed

--- myRegex.py
+++ myRegex.py
@@ -1,4 +1,5 @@
 import re
+from datetime import datetime, timedelta
 
 # test_list = [
 #     '提醒 今天下午14点干一件事',
@@ -17,6 +18,83 @@ re_time = re.compile(pattern)
 # group7: 上午 下午
 # group8: 点
 
-# m = re_time.match(test_list[0])
-# print(m.group(7))
+def parse_input(ctx, action):
+    m = re_time.match(ctx.Content)
+    currentTime = datetime.now()
+    msgDate = None  # Year, month, day
+    msgTime = None # minute, 
+    if(m!=None):
+        if(m.group(2) != None and m.group(3) != None):
+            action.sendFriendText(ctx.FromUin, "不能同时出现日期和今天明天啦！")
+            return None
+        
+        if(m.group(2) == None and m.group(3) == None):
+            msgDate = currentTime
+        if(m.group(2) != None and m.group(3) == None):
+            if(m.group(2) == "明天"):
+                msgDate = currentTime + timedelta(days=1)
+            if(m.group(2) == "后天"):
+                msgDate = currentTime + timedelta(days=2)
+            if(m.group(2) == "大后天"):
+                msgDate = currentTime + timedelta(days=3)
+        else:
+            year = 0
+            month = 0
+            day = 0
+            if(m.group(4) != None):
+                year = int(m.group(4)[:-1])
+            else:
+                year = currentTime.year
+            if(m.group(5) != None):
+                month = int(m.group(5)[:-1])
+            else:
+                month = currentTime.month
+            if(m.group(6) != None):
+                if(m.group(6)[-1] == '日'):
+                    day = int(m.group(6)[:-1])
+                else:
+                    day = int(m.group(6))
+            else:
+                day = currentTime.day
 
+            try:
+                msgDate = datetime(year, month, day)
+            except ValueError as e:
+                action.sendFriendText(ctx.FromUin, str(e))
+                return None
+
+        if(m.group(7) == None and group(8) == None):
+            msgTime = datetime(2000, 1, 1, 20)
+        elif(m.group(8) == None):
+            if(m.group(7) == "上午"):
+                msgTime = datetime(2000, 1, 1, 12)
+            else:
+                msgTime = datetime(2000, 1, 1, 20)
+        elif(m.group(7) == None):
+            tmp = int(m.group(7)[:-1])
+            try:
+                msgTime = datetime(2000, 1, 1, tmp)
+            except ValueError as e:
+                action.sendFriendText(ctx.FromUin, str(e))
+                return None
+        else:
+            tmp = int(m.group(8)[:-1])
+            if(m.group(7) == "上午"):
+                if(tmp > 12):
+                    action.sendFriendText(ctx.FromUin, "上午怎么可能超过12点嘛！")
+                    return None
+                msgTime = datetime(2000, 1, 1, tmp)
+            else:
+                if(tmp < 12):
+                    msgTime = datetime(2000, 1, 1, tmp+12)
+                else:
+                    try:
+                        msgTime = datetime(2000, 1, 1, tmp)
+                    except ValueError as e:
+                        action.sendFriendText(ctx.FromUin, str(e))
+                        return None
+        msgDate = msgDate.replace(hour=msgTime.hour, minute=0, second=0, microsecond=0)
+        if msgDate < currentTime:
+            action.sendFriendText(ctx.FromUin, "时间已经过了啦...")
+            return None
+        return msgDate
--- todoBot.py
+++ todoBot.py
@@ -1,8 +1,8 @@
 from botoy import Action, Botoy, GroupMsg
 from botoy import decorators as deco
 import os
-from datetime import datetime, timedelta
-from myRegex import re_time
+
+from myRegex import parse_input
 
 bot = Botoy()
 masterQQ = os.environ['targetQQ']
@@ -19,82 +19,10 @@ def _(ctx):
 def get_msg(ctx):
     action = Action(ctx.CurrentQQ)
 
-    
-    m = re_time.match(ctx.Content)
-    currentTime = datetime.now()
-    msgDate = None  # Year, month, day
-    msgTime = None # minute, 
-    if(m!=None):
-        if(m.group(2) != None and m.group(3) != None):
-            action.sendFriendText(ctx.FromUin, "不能同时出现日期和今天明天啦！")
-            return
-        
-        if(m.group(2) == None and m.group(3) == None):
-            msgDate = currentTime
-        if(m.group(2) != None and m.group(3) == None):
-            if(m.group(2) == "明天"):
-                msgDate = currentTime + timedelta(days=1)
-            if(m.group(2) == "后天"):
-                msgDate = currentTime + timedelta(days=2)
-            if(m.group(2) == "大后天"):
-                msgDate = currentTime + timedelta(days=3)
-        else:
-            year = 0
-            month = 0
-            day = 0
-            if(m.group(4) != None):
-                year = int(m.group(4)[:-1])
-            else:
-                year = currentTime.year
-            if(m.group(5) != None):
-                month = int(m.group(5)[:-1])
-                print("MONTH GET")
-            else:
-                month = currentTime.month
-                print(m.group(5))
-                print("NO MONTH")
-            if(m.group(6) != None):
-                if(m.group(6)[-1] == '日'):
-                    day = int(m.group(6)[:-1])
-                else:
-                    day = int(m.group(6))
-            else:
-                day = currentTime.day
-
-            try:
-                msgDate = datetime(year, month, day)
-            except ValueError as e:
-                action.sendFriendText(ctx.FromUin, str(e))
-                return
-
-        if(m.group(7) == None and group(8) == None):
-            msgTime = datetime(2000, 1, 1, 20)
-        elif(m.group(8) == None):
-            if(m.group(7) == "上午"):
-                msgTime = datetime(2000, 1, 1, 12)
-            else:
-                msgTime = datetime(2000, 1, 1, 20)
-        elif(m.group(7) == None):
-            tmp = int(m.group(7)[:-1])
-            try:
-                msgTime = datetime(2000, 1, 1, tmp)
-            except ValueError as e:
-                action.sendFriendText(ctx.FromUin, str(e))
-                return
-        else:
-            tmp = int(m.group(8)[:-1])
-            if(m.group(7) == "上午"):
-                if(tmp > 12):
-                    action.sendFriendText(ctx.FromUin, "上午怎么可能超过12点嘛！")
-                    return
-                msgTime = datetime(2000, 1, 1, tmp)
-            else:
-                if(tmp < 12):
-                    msgTime = datetime(2000, 1, 1, tmp+12)
-                else:
-                    msgTime = datetime(2000, 1, 1, tmp)
-        msgDate = msgDate.replace(hour=msgTime.hour, minute=0, second=0, microsecond=0)
-        action.sendFriendText(ctx.FromUin, str(msgDate))
+    msgDate = parse_input(ctx, action)
+    if msgDate is None:
+        return
+    action.sendFriendText(ctx.FromUin, str(msgDate))
 
 
             
